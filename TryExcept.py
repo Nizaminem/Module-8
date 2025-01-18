0b644039d3c870f37a05c03a8fb076f5ace70cf1
Nizaminem <nagarizaev1994@gmail.com>
2025-01-18 13:02:39 +0300

    Программистам всё можно

---
+++ TryExcept.py
@@ -0,0 +1,14 @@
+
+def add_everything_up(a,b):
+    try:
+        x = a + b
+        return round(x, 3)
+    except TypeError:
+        return str(a)+str(b)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+
+
